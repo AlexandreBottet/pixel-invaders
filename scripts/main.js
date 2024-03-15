548a0d636a9eeb7e8a25895806f2c02880bb3350
AlexandreBottet <alexandre.bottet@outlook.fr>
2024-03-15 13:33:05 +0100

    feat: Extend creation of a grid with possibility of changing cell size

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,12 @@ const app = {
     pixelGrid: document.getElementById('pixel-grid'),
     form: document.getElementById('configuration'),
     sizeInput: null,
+    pixelInput: null,
     button: null,
     numOfRows: 8,
     numOfColumns: 8,
+    pixelWidth: '15px',
+    pixelHeigth: '15px',
 
     createGrid() {
         while (this.pixelGrid.firstChild) {
@@ -26,6 +29,8 @@ const app = {
     createCell() {
         const cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.style.width = this.pixelWidth;
+        cell.style.height = this.pixelHeigth;
     
         cell.addEventListener('click', () => {
             const currentColor = cell.style.backgroundColor;
@@ -37,15 +42,29 @@ const app = {
     createInputContainer() {
         const inputContainer = document.createElement('div');
         inputContainer.classList.add('input-container');
+
         this.sizeInput = document.createElement('input');
         this.sizeInput.classList.add('size-input');
         this.sizeInput.placeholder = 'Taille de la grille';
+        
+        this.pixelInput = document.createElement('input');
+        this.pixelInput.classList.add('pixel-input');
+        this.pixelInput.placeholder = 'Taille des pixels';
+
         this.button = document.createElement('button');
         this.button.innerText = 'Valider';
-        this.button.addEventListener('click', this.changeSizeOfGrid.bind(this));
+
         inputContainer.appendChild(this.sizeInput);
+        inputContainer.appendChild(this.pixelInput)
         inputContainer.appendChild(this.button);
         this.form.appendChild(inputContainer);
+
+        this.form.addEventListener('submit', e => {
+            e.preventDefault();
+            this.changeSizeOfGrid();
+            this.changePixelSize();
+            this.createGrid();
+        })
     },
 
     changeSizeOfGrid() {
@@ -54,7 +73,6 @@ const app = {
         if (newSize <= 45) {
             this.numOfRows = newSize;
             this.numOfColumns = newSize;
-            this.createGrid();
         } else {
             const errorMessage = document.createElement('p');
             errorMessage.textContent = 'Il ne faut pas abuser des bonnes choses. S\'il te plaît, veille à ne rentrer qu\'une valeur inférieure à 46. Merci.';
@@ -64,6 +82,13 @@ const app = {
                 this.form.removeChild(errorMessage);
             }, 5000)
         }
+    },
+    
+    changePixelSize() {
+        const newSize = this.pixelInput.value;
+
+        this.pixelWidth = `${newSize}px`;
+        this.pixelHeigth = `${newSize}px`;    
     },
 
     init() {
